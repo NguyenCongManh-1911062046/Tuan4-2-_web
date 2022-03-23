d42ba51ddc92dc90af3a798fca674a8b40c57c10
HACKER <HACKER@DESKTOP-UJQO0L3>
2022-03-23 17:46:45 +0700

    Add project files.

---
+++ Tuan4_web/Models/GiohangBase.cs
@@ -0,0 +1,12 @@
+﻿namespace Tuan4_web.Models
+{
+    public abstract class GiohangBase
+    {
+        public abstract int masach { get; set; }
+        public abstract string tensach { get; set; }
+        public abstract string hinh { get; set; }
+        public abstract double giaban { get; set; }
+        public abstract int iSoluong { get; set; }
+        public abstract double dThanhtien { get; }
+    }
+}
